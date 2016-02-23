d9559e2ef51f1e0da9adb3b6d9aa88aee352197c
austinjkovach <kovach.a.j@gmail.com>
2016-02-23 14:17:18 -0600

    Promisified https.get

--- server/BnetHelper.js
+++ server/BnetHelper.js
@@ -1,33 +1,91 @@
 var https = require('https');
 var Promise = require('bluebird');
 
-var getLadderInfoPromise = function(url){
-  return new Promise(function(resolve){
+var PromiseGet = Promise.promisify(require('https').get);
 
-  });
-}
+var getLadderInfoP = function(ladderId){
+  return PromiseGet('https://us.api.battle.net/sc2/ladder/' + ladderId + '?locale=en_US&apikey=' + process.env.BNET_KEY, function(response){
+  var fullBody = '';
+  response.setEncoding('utf8');
 
-// getProfile('string');
+  response.on('data', (chunk) => {
+    console.log('got chunk:', chunk);
+    fullBody += chunk;
+  })
+  .on('end', () => {
+    return fullBody;
+  })
+})
+  // .then(function(result){
+  //   console.log(result);
+  // })
+  // .catch(function(error){
+  //   console.log('error:', error)
+  // })
+};
 
-
-
-var getLadderInfo = function(ladderId){
-  https.get('https://us.api.battle.net/sc2/ladder/' + ladderId + '?locale=en_US&apikey=' + process.env.BNET_KEY, (response) =>{
+var getProfileInfoP = function(profileId, profileName, regionId){
+  var regionId = regionId ? regionId : 1;
+  return PromiseGet('https://us.api.battle.net/sc2/profile/' + profileId + '/' + regionId + '/' + profileName + '/?locale=en_US&apikey=' + process.env.BNET_KEY, function(response){
 
     var fullBody = '';
     response.setEncoding('utf8');
 
-    response.on('data', (chunk) => {
-      fullBody += chunk;
+    response.on('data', (data) => {
+      fullBody += data;
     })
-    .on('end', () => {
-      console.log('got:', fullBody);
+    .on('end', (data) => {
+      fullBody = JSON.parse(fullBody);
+      console.log('full:', fullBody)
       return fullBody;
-    })
-  })
-  .on('error', function(){
-    console.log('error:', error);
+    });
   });
 }
 
-getLadderInfo(18979);
+getProfileInfoP(3248629, 'Bazooka');
+
+// getLadderInfoP()
+// var getLadderInfo = function(ladderId){
+//   https.get('https://us.api.battle.net/sc2/ladder/' + ladderId + '?locale=en_US&apikey=' + process.env.BNET_KEY, (response) =>{
+
+//     var fullBody = '';
+//     response.setEncoding('utf8');
+
+//     response.on('data', (chunk) => {
+//       fullBody += chunk;
+//     })
+//     .on('end', () => {
+//       console.log('got:', fullBody);
+//       return fullBody;
+//     })
+//   })
+//   .on('error', function(){
+//     console.log('error:', error);
+//   });
+// }
+
+// getLadderInfo(18979);
+
+/*
+
+  - Blizzard API
+  https://dev.battle.net/io-docs
+  http://bluebirdjs.com/docs/api/new-promise.html
+  https://nodejs.org/api/https.html#https_https_get_options_callback
+  http://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
+  http://stackoverflow.com/questions/31098009/node-js-request-node-js-bluebird-request
+  https://github.com/petkaantonov/bluebird/issues/196
+
+    +++ Promisify request 
+    - Add Profile request
+    - Add Profile Ladder request
+
+  - Postgres DB
+    - Add request data to DB
+    - Add from DB for info
+
+  - Show Data
+    - Load script tags
+    - Browserify?
+
+*/
